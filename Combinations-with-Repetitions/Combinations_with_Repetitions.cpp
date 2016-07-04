c893af1f7f4ee0d2bfc212913c7464ee0d3484c7
Rachit Belwariar <gbelwariar@gmail.com>
2016-07-04 16:50:13 +0530

    Add files via upload

---
+++ Combinations-with-Repetitions/Combinations_with_Repetitions.cpp
@@ -0,0 +1,55 @@
+// Program to print all combination of size r in an array
+// of size n with repetitions allowed
+#include <stdio.h>
+ 
+/* arr[]  ---> Input Array
+  chosen[] ---> Temporary array to store indices of
+                current combination
+   start & end ---> Staring and Ending indexes in arr[]
+   r ---> Size of a combination to be printed */
+void CombinationRepetitionUtil(int chosen[], int arr[],
+                    int index, int r, int start, int end)
+{
+    // Since index has become r, current combination is
+    // ready to be printed, print
+    if (index == r)
+    {
+        for (int i = 0; i < r; i++)
+            printf("%d ", arr[chosen[i]]);
+        printf("\n");
+        return;
+    }
+ 
+    // One by one choose all elements (without considering
+    // the fact whether element is already chosen or not)
+    // and recur
+    for (int i = start; i <= end; i++)
+    {
+        chosen[index] = i;
+        CombinationRepetitionUtil(chosen, arr, index + 1,
+                                               r, i, end);
+    }
+    return;
+}
+ 
+// The main function that prints all combinations of size r
+// in arr[] of size n with repitions. This function mainly
+// uses CombinationRepetitionUtil()
+void CombinationRepetition(int arr[], int n, int r)
+{
+    // Allocate memory
+    int chosen[r+1];
+ 
+    // Call the recursice function
+    CombinationRepetitionUtil(chosen, arr, 0, r, 0, n-1);
+}
+ 
+// Driver program to test above functions
+int main()
+{
+    int arr[] = {1, 2, 3, 4};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int r = 2;
+    CombinationRepetition(arr, n, r);
+    return 0;
+}
