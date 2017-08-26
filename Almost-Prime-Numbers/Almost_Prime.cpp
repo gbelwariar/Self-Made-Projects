77f2f65f9188b50ae1d38bb256a305db23d1dc83
Rachit Belwariar <gbelwariar@gmail.com>
2017-08-26 12:54:18 +0530

    Code formatting.

--- Almost-Prime-Numbers/Almost_Prime.cpp
+++ Almost-Prime-Numbers/Almost_Prime.cpp
@@ -7,31 +7,28 @@
 int countPrimeFactors(int n)
 {
 	int count = 0;
-    // Count the number of 2s that divide n
-    while (n%2 == 0)
-    {
-        n = n/2;
-        count++;
-    }
-
-    // n must be odd at this point. So we can skip one
+    	// Count the number of 2s that divide n
+	while (n%2 == 0)
+    	{
+        	n = n/2;
+        	count++;
+    	}
+    	// n must be odd at this point. So we can skip one
 	// element (Note i = i +2)
-    for (int i = 3; i <= sqrt(n); i = i+2)
-    { 
-        // While i divides n, count i and divide n
-        while (n%i == 0)
-        {
-            n = n/i;
-            count++;
-        }
-    }
-
-    // This condition is to handle the case whien n is a
+    	for (int i = 3; i <= sqrt(n); i = i+2)
+    	{ 
+        	// While i divides n, count i and divide n
+        	while (n%i == 0)
+        	{
+			n = n/i;
+			count++;
+	        }
+	}
+	// This condition is to handle the case whien n is a
 	// prime number greater than 2
-    if (n > 2)
-        count++;
-   
-   return(count);     
+	if (n > 2)
+		count++;
+	return(count);     
 }
 
 // A function to print the first 10 k-almost primes
@@ -40,7 +37,6 @@ void printKAlmostPrimes(int k)
 {
 	int i, num;
 	printf("First ten %d-almost prime numbers- ", k);
-	
 	for (i=1, num=2; i<=10; num++)
 	{
 		if (countPrimeFactors(num) == k)
@@ -50,7 +46,6 @@ void printKAlmostPrimes(int k)
 		}
 	}
 	printf("\n");
-	
 	return;
 }
 
@@ -60,6 +55,5 @@ int main()
 {
 	for (int k=1; k<=5; k++)
 		printKAlmostPrimes(k);
-   
     return 0;
 }
